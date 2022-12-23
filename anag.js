e722d92c2b5e6e759d06541dfa1a6434d7410251
mark <tranter.m@sky.com>
2022-12-23 20:57:50 +0000

    first push

---
+++ anag.js
@@ -0,0 +1,77 @@
+const { dictionary } = require("./newDict.js");
+
+const freq = (word) => {
+  let obj = {};
+  Array.from(word).forEach((c) => {
+    obj[c] = obj[c] ? obj[c] + 1 : 1;
+  });
+  return obj;
+};
+
+const remover = (freq, word) => {
+  let temp = structuredClone(freq);
+  word.split("").forEach((e) => {
+    temp[e] -= 1;
+  });
+  return temp;
+};
+
+const isSubString = (origFreq, candFreq) => {
+  return Object.keys(candFreq).every((k) => origFreq[k] >= candFreq[k]);
+};
+
+const multi = (str, wordLens) => {
+  let arr = [...wordLens].sort().reverse();
+  const sols = [];
+  const findSols = (strFreq, cands, candInd, wordInd, sol) => {
+    if (cands.length === 0) {
+      return;
+    }
+    let tempSol = sol.concat(cands[candInd].word);
+    if (tempSol.length === arr.length) {
+      sols.push(tempSol);
+    }
+    if (candInd < cands.length - 1) {
+      findSols(strFreq, cands, candInd + 1, wordInd, sol);
+    }
+    if (wordInd < arr.length - 1) {
+      let tempFreq = remover(strFreq, cands[candInd].word);
+      findSols(
+        tempFreq,
+        dictionary[arr[wordInd + 1] - 1].filter((e) =>
+          isSubString(tempFreq, e.f)
+        ),
+        0,
+        wordInd + 1,
+        tempSol
+      );
+    }
+  };
+
+  let strFreq = freq(str);
+  findSols(
+    strFreq,
+    dictionary[arr[0] - 1].filter((e) => isSubString(strFreq, e.f)),
+    0,
+    0,
+    []
+  );
+  return sols;
+};
+
+const origOrder = (wordLengths, sol) => {
+  res = [];
+  wordLengths.forEach((w) => {
+    let temp = sol.findIndex((e) => e.length === w);
+    res.push(sol[temp]);
+    sol.splice(temp, 1);
+  });
+  return res.join(" ");
+};
+
+let wordLengths = [6, 6, 7];
+let res = multi("dsrsiagoeuemionnrsc", wordLengths);
+res.sort(function (a, b) {
+  return a.localeCompare(b);
+});
+res.forEach((e) => console.log(origOrder(wordLengths, e)));
