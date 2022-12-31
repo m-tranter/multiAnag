36c37c8fb4e24dd305896c8e8ec5e9062f806da0
mark <tranter.m@sky.com>
2022-12-31 18:14:40 +0000

    Added some comments.

--- anag.js
+++ anag.js
@@ -4,6 +4,7 @@ const lengthsInput = document.getElementById("lengthsInput");
 const myBtn = document.getElementById("goBtn");
 const display = document.getElementById("display");
 
+// Put a solution in the order supplied by user.
 const origOrder = (wordLengths, sol) => {
   return wordLengths
     .reduce((acc, w) => {
@@ -15,6 +16,7 @@ const origOrder = (wordLengths, sol) => {
     .join(" ");
 };
 
+// Create an HTML list to display.
 const makeList = (array) => {
   const list = document.createElement("ul");
   list.classList.add("list-unstyled");
@@ -26,26 +28,35 @@ const makeList = (array) => {
   return list;
 };
 
+// Returns an H2.
 const makeH2 = (str) => {
   const h2 = document.createElement("h2");
   h2.innerText = str;
   return h2;
 };
 
+// Main function - processes inputs and displays results.
 const findWords = () => {
-  let str = strInput.value.replace(/[^A-Za-z\s]/g, "").toLowerCase();
-  let pattern = patternInput.value.toLowerCase().replace(/\s/g, "").split(",");
-  let lengths;
+  display.innerHTML = "";
+  const str = strInput.value.replace(/[^A-Za-z\s]/g, "").toLowerCase();
+  const pattern = patternInput.value
+    .toLowerCase()
+    .replace(/\s/g, "")
+    .split(",");
+  let lengths = lengthsInput.value.split(",").map((e) => parseInt(e));
   let info;
-  lengths = lengthsInput.value.split(",").map((e) => parseInt(e));
-  if (str.length === 0) {
+  // Can't proceed without letters to process.
+  if (!str.length) {
     display.appendChild(makeH2("Please enter some letters."));
     return;
   }
-  if (isNaN(lengths[0])) {
+  // Catches errors in the lengths field.
+  // Default to looking for one word answers.
+  if (lengths.some((e) => isNaN(e))) {
     lengths = [str.length];
     info = [{ len: str.length, pattern: "" }];
   } else {
+    // Make the array of objects made of lengths & patterns.
     info = processPatterns(lengths, pattern);
     info.sort(compare);
   }
@@ -56,7 +67,6 @@ const findWords = () => {
       acc.push(origOrder(lengths, sol));
       return acc;
     }, []);
-    res.sort();
     display.innerHTML = "";
     display.appendChild(res.length ? makeList(res) : makeH2("Nothing found."));
   }, 0);
@@ -66,15 +76,10 @@ const sumArr = (arr) => arr.reduce((acc, v) => acc + v);
 
 const sumLens = (arr) => arr.reduce((acc, w) => acc + w.length, 0);
 
+// Make array of objects with lengths & patterns.
 const processPatterns = (wordLengths, pattern) => {
   if (sumLens(pattern) !== sumArr(wordLengths)) {
-    return wordLengths.reduce((acc, v) => {
-      acc.push({
-        len: v,
-        pattern: "",
-      });
-      return acc;
-    }, []);
+    pattern = Array(wordLengths.length).fill("");
   }
   return wordLengths.reduce((acc, v, i) => {
     acc.push({
@@ -87,6 +92,7 @@ const processPatterns = (wordLengths, pattern) => {
   }, []);
 };
 
+// Order by length of pattern (if supplied) & then length of word.
 const compare = (a, b) => {
   if (a.pattern.length > b.pattern.length) {
     return -1;
@@ -102,6 +108,8 @@ const compare = (a, b) => {
   }
   return 0;
 };
+
+// Create a frequency table for a string.
 const freq = (word) => {
   return Array.from(word).reduce((f, ch) => {
     f[ch] = f[ch] ? f[ch] + 1 : 1;
@@ -109,6 +117,7 @@ const freq = (word) => {
   }, {});
 };
 
+// Reduce frequency counts for all letters in word.
 const remover = (freq, word) => {
   return Array.from(word).reduce(
     (f, ch) => {
@@ -119,25 +128,31 @@ const remover = (freq, word) => {
   );
 };
 
-const isMatch = (pattern, word) => {
+// Returns a function that takes one argument.
+// It checks if the input is a match for the pattern supplied here.
+const isMatch = (pattern) => {
   const re = new RegExp(`${pattern.replace(/\?/g, ".")}`);
-  return word.match(re) !== null;
+  return (word) => word.match(re) !== null;
 };
 
+// Tests if the candidate is a substring of the frequency passed.
 const isSubString = (origFreq, candFreq) => {
   return Object.keys(candFreq).every((k) => origFreq[k] >= candFreq[k]);
 };
 
+// Looks recursively through the dictionary for matches.
 const multi = (str, arr) => {
+  // Returns a list of words that match.
   const getCands = (obj, strFreq) => {
     if (obj.pattern.length > 0) {
       return dictionary[obj.len - 1].filter(
-        (e) => isSubString(strFreq, e.f) && isMatch(obj.pattern, e.word)
+        (e) => isSubString(strFreq, e.f) && isMatch(obj.pattern)(e.word)
       );
     }
     return dictionary[obj.len - 1].filter((e) => isSubString(strFreq, e.f));
   };
 
+  // This gets called recursively. Builds up solutions.
   const findSols = (strFreq, cands, candInd, wordInd, sol) => {
     if (!cands.length) {
       return;
